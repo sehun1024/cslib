3db3320e8a09bfa56356f82f56d9d6290df59319
Elimia <sehun1024@gmail.com>
2026-02-16 14:55:39 +0900

    The code was modified to meet the convention of mathlib

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/LC_Equivalence.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/LC_Equivalence.lean
@@ -1,8 +1,21 @@
+/-
+Copyright (c) 2026 Elimia (Sehun Kim). All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Elimia (Sehun Kim)
+-/
+
 module
 
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Properties
 
 public section
+
+/-!
+
+This section is for the alternative and more useful definition for the "locally closed".
+The equivalence of the definitions will be proven in this section.
+
+-/
 
 namespace Cslib.LambdaCalculus.LocallyNameless.Untyped.Term
 
@@ -10,172 +23,85 @@ universe u
 
 variable {Var : Type u}
 
-/-!
-
-This section is for the alternative and more useful definition for the "locally closed".
-
--/
-
 /-- Alternative Definitions for LC
-For all bound variables, lc_at looks for the binding lambda is actually there.
+For each bound variable, lcAt checks whether the corresponding binding lambda exists.
 
 We can define M is a locally closed term if and only if
 
 ```
-  lc_at 0 M
+  lcAt 0 M
 ```
-
-The equivalence of the definitions will be proven in this section.
 -/
-@[simp]
-def lc_at (k : ℕ) (M : Term Var):Prop :=
-match M with
-| Term.bvar i => i<k
-| Term.fvar _ => True
-| Term.app t₁ t₂ => lc_at k t₁ ∧ lc_at k t₂
-| Term.abs t => lc_at (k+1) t
+@[simp, scoped grind =]
+def lcAt (k : ℕ) : Term Var → Prop
+| bvar i => i<k
+| fvar _ => True
+| app t₁ t₂ => lcAt k t₁ ∧ lcAt k t₂
+| abs t => lcAt (k+1) t
 
 /-- depth counts the maximum number of the lambda that is enclosing variables. -/
-@[simp]
-def depth (M : Term Var) : ℕ :=
-match M with
-| Term.bvar _ => 0
-| Term.fvar _ => 0
-| Term.app t₁ t₂ => Nat.max (depth t₁) (depth t₂)
-| Term.abs t => depth t +1
+@[simp, scoped grind =]
+def depth : Term Var → ℕ
+| bvar _ => 0
+| fvar _ => 0
+| app t₁ t₂ => max (depth t₁) (depth t₂)
+| abs t => depth t +1
 
-
+@[elab_as_elim]
 protected lemma ind_on_depth (P : Term Var → Prop)
-(h1 : ∀ i, P (bvar i))
-(h2 : ∀ x, P (fvar x))
-(h3 : ∀ M N, P M → P N → P (app M N))
-(h4 : ∀ M, P M → (∀ N, depth N ≤ depth M → P N) → P M.abs)
+(h_bvar : ∀ i, P (bvar i))
+(h_fvar : ∀ x, P (fvar x))
+(h_app : ∀ M N, P M → P N → P (app M N))
+(h_abs : ∀ M, P M → (∀ N, N.depth ≤ M.depth → P N) → P M.abs)
 : ∀ M, P M := by
-  have h : ∀ (d: ℕ) (M:Term Var), depth M ≤ d → P M := by
+  have h : ∀ (d: ℕ) (M:Term Var), M.depth ≤ d → P M := by
     intros d;induction d
     case zero =>
-      intros M h5
-      induction M
-      case bvar => solve_by_elim
-      case fvar => solve_by_elim
-      case abs M' p => simp at h5
-      case app x y p q=>
-        simp at h5
-        grind
+      intros M;induction M <;>grind
     case succ m h' =>
-      intros M p;induction M
-      case bvar => solve_by_elim
-      case fvar => solve_by_elim
-      case abs M' q =>
-        simp at p
-        apply h4 M'
-        · apply q;grind
+      intros M p;induction M with
+      | abs M' q =>
+        apply h_abs M'
+        · grind
         · intros N h2
-          have h3: depth N ≤ m := by grind
+          have h3: N.depth ≤ m := by grind
           exact h' N h3
-      case app x y q r =>
+      | app x y q r =>
         simp only [depth, sup_le_iff] at p
-        exact h3 _ _ (q p.1) (r p.2)
+        exact h_app _ _ (q p.1) (r p.2)
+      | _ => grind
   exact (fun M ↦ h (depth M) M (by constructor))
-
 
 /-- The depth of the lambda expression doesn't change by opening at i-th bound variable
  for some free variable. -/
-lemma depth_eq_depth_openRec_fvar (M : Term Var) (x : Var) :
-∀ (i : ℕ), depth (@openRec Var i (fvar x) M)= depth M := by
-  induction M
-  case bvar j =>
-    intros i;simp only [depth, openRec]
-    have h : (if i = j then fvar x else bvar j) = fvar x
-      ∨ (if i = j then fvar x else bvar j) = bvar j := ite_eq_or_eq _ _ _
-    induction h
-    case inl h' => rw [h'];rfl
-    case inr h' => rw [h'];rfl
-  case fvar y => intros i;simp only [depth, openRec]
-  case app t₁ t₂ p₁ p₂ =>
-    intros i;simp only [depth, openRec]
-    rw [p₁,p₂]
-  case abs t p =>
-    intros i;simp only [depth, openRec, Nat.add_right_cancel_iff]
-    exact p (i+1)
+ @[simp, scoped grind .]
+lemma depth_openRec_fvar_eq_depth (M : Term Var) (x : Var) (i : ℕ) :
+  (M⟦i ↝ fvar x⟧).depth = M.depth := by
+  induction M generalizing i <;> grind
 
 /-- The depth of the lambda expression doesn't change by opening for some free variable. -/
-theorem depth_eq_depth_open_fvar (M : Term Var) (x : Var) :
-depth (M ^ fvar x) = depth M := depth_eq_depth_openRec_fvar M x 0
+theorem depth_open_fvar_eq_depth (M : Term Var) (x : Var) :
+  depth (M ^ fvar x) = depth M := depth_openRec_fvar_eq_depth M x 0
 
+/-- Opening for some free variable at i-th bound variable, increases the lcAt by 1. -/
+@[simp, scoped grind .]
+theorem lcAt_openRec_fvar_iff_lcAt (M : Term Var) (x : Var) (i : ℕ) :
+  lcAt i (M⟦i ↝ fvar x⟧) ↔ lcAt (i+1) M := by
+  induction M generalizing i <;> grind
 
-/-- Opening for some free variable at i-th bound variable, increases the lc_at by 1. -/
-theorem lc_at_iff_lc_at_openRec_fvar (M : Term Var) (x : Var) (i : ℕ) :
-  lc_at (i+1) M ↔ lc_at i (@openRec Var i (fvar x) M)
-:= by
-  revert i
-  induction M
-  case bvar k =>
-    intros i; simp only [lc_at, Order.lt_add_one_iff]
-    simp only [openRec]
-    have p1:= ite_eq_iff.mp (@rfl _ (if i = k then fvar x else bvar k))
-    obtain ⟨p2,p3⟩|⟨p2,p3⟩ := p1
-    · rw [← p3];simp;grind
-    · rw [← p3];simp;grind
-  case fvar x =>
-    simp only [openRec]
-    intros;simp
-  case app x y p q =>
-    simp only [openRec,lc_at]
-    intros i
-    grind
-  case abs M p =>
-    intros i
-    simp only [lc_at]
-    solve_by_elim
+/-- Opening for some free variable is locally closed if and only if M is lcAt 1. -/
+theorem lcAt_open_fvar_iff_lcAt (M : Term Var) (x : Var) :
+   lcAt 0 (M ^ (fvar x)) ↔ lcAt 1 M := lcAt_openRec_fvar_iff_lcAt M x 0
 
-
-/-- Opening for some free variable is locally closed if and only if M is lc_at 1. -/
-theorem lc_at_iff_lc_at_open_fvar (M : Term Var) (x : Var) :
-  lc_at 1 M ↔ lc_at 0 (M ^ (fvar x)) := lc_at_iff_lc_at_openRec_fvar M x 0
-
-
-/-- M is lc_at 0 if and only if M is locallly closed -/
-theorem lc_at_iff_LC (M : Term Var) [hF : HasFresh Var] : lc_at 0 M ↔ M.LC := by
-  apply LambdaCalculus.LocallyNameless.Untyped.Term.ind_on_depth (fun M ↦ lc_at 0 M ↔ M.LC )
-  case h1 =>
-    simp only [lc_at, not_lt_zero, false_iff];intros i p
-    rcases p
-  case h2 =>
-    simp only [lc_at, true_iff];intros x
+/-- M is lcAt 0 if and only if M is locallly closed -/
+theorem lcAt_iff_LC (M : Term Var) [hF : HasFresh Var] : lcAt 0 M ↔ M.LC := by
+induction M using LambdaCalculus.LocallyNameless.Untyped.Term.ind_on_depth with
+  | h_abs =>
     constructor
-  case h3 =>
-    simp only [lc_at];intros M N p q
-    constructor
-    · intros h
-      constructor
-      · grind
-      · grind
-    · intros h
-      rcases h with ⟨h1,h2 ⟩
-      grind
-  case h4 =>
-  intros M' ha1 ha2
-  simp only [lc_at, zero_add]
-  constructor
-  · intros h2
-    apply LC.abs ∅
-    intros x _
-    have ha3 := lc_at_iff_lc_at_openRec_fvar M' x 0
-    simp at ha3
-    have ha4 := depth_eq_depth_open_fvar M' x
-    exact (ha2 (M' ^ fvar x) (by grind)).mp (ha3.mp h2)
-  · intros h2
-    rcases h2 with ⟨⟩|⟨L,_,q⟩
-    let y:= hF.fresh L
-    let h3 := hF.fresh_notMem L
-    have h4 := q y h3
-    have h5 : depth (M' ^ fvar y) = depth M' := by
-      simp only [open']
-      rw [depth_eq_depth_openRec_fvar M' y]
-    have h6 := (ha2 (M' ^ fvar y) (by grind))
-    have h7 := h6.mpr h4
-    rw [lc_at_iff_lc_at_openRec_fvar M' y]
-    exact h7
+    · grind [LC.abs ∅]
+    · intros h2
+      rcases h2 with ⟨⟩|⟨L,_,_⟩
+      grind [fresh_exists L]
+  | _ => grind [cases LC]
 
 end Cslib.LambdaCalculus.LocallyNameless.Untyped.Term
